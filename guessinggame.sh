ae0863eb5daa583900e75967e0c5116b73d5c935
m01nak <64699276+m01nak@users.noreply.github.com>
2020-06-02 13:05:07 +0530

    updated guessinggame.sh and makefile

--- guessinggame.sh
+++ guessinggame.sh
@@ -1,2 +1,20 @@
-1
-2
+guessinggame() {
+	flag=0
+	rcnt=$(ls | awk 'END {print NR}')
+	while [[ $flag -eq 0 ]]
+	do
+		echo "How many files in this directory? Take a guess..."
+		read gcnt
+		if [[ $gcnt -eq $rcnt ]]
+		then
+			echo "Congratulations! You are correct!"
+			let flag=1
+		elif [[ $gcnt -lt $rcnt ]]
+		then
+			echo "You guessed too low! Try again."
+		else [[ $gcnt -gt $rcnt ]]
+			echo "You guessed too high! Try again."
+		fi
+	done
+}
+guessinggame
--- makefile
+++ makefile
@@ -1,5 +1,4 @@
 all:
-	touch README.md
-	echo "guessinggame.sh by Moinak Dasgupta" >> README.md
+	echo "guessinggame.sh by Moinak Dasgupta" > README.md
 	date >> README.md
 	awk 'END {print NR}' guessinggame.sh >> README.md
